8b70d64eeed1b467d30b5aed0543d2539e1cb211
Thomas Wessel <turpleturtle12@gmail.com>
2024-10-05 21:35:52 -0700

    Step 2: Count the number of button presses

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Shell Tapper";
+let clickCount = 0;
 
 document.title = gameName;
 
@@ -13,3 +14,13 @@ app.append(header);
 const button = document.createElement("button");
 button.innerHTML = "🐢";
 app.append(button);
+
+const clickCountDisplay = document.createElement("p");
+clickCountDisplay.innerHTML = `You've tapped the turtle ${clickCount} times.`;
+app.append(clickCountDisplay);
+
+//event listener for button click
+button.addEventListener("click", () => {
+  clickCount++;
+  clickCountDisplay.innerHTML = `You've tapped the turtle ${clickCount} times.`;
+});
