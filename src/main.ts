6cb001809bc6d096173d78a565ba42ad3743f7a8
Thomas Wessel <turpleturtle12@gmail.com>
2024-10-06 20:08:51 -0700

    Complete step 5

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Shell Tapper";
 let clickCount = 0;
+let growthRate = 0;
 
 document.title = gameName;
 
@@ -13,10 +14,21 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const button = document.createElement("button");
-button.innerHTML = "🐢";
-button.addEventListener("click", () => {incrementClickCount(1)});
-app.append(button);
+const mainClickButton = document.createElement("button");
+mainClickButton.innerHTML = "🐢";
+mainClickButton.addEventListener("click", () => {
+    incrementClickCount(1)
+});
+app.append(mainClickButton);
+
+const marioUpgradeButton = document.createElement("button");
+marioUpgradeButton.innerHTML = "Mario";
+marioUpgradeButton.disabled = true;
+marioUpgradeButton.addEventListener("click", () => {
+    growthRate++;
+    clickCount -= 10;
+});
+app.append(marioUpgradeButton);
 
 const clickCountDisplay = document.createElement("p");
 clickCountDisplay.innerHTML = `You've tapped the turtle ${clickCount} times.`;
@@ -24,22 +36,25 @@ app.append(clickCountDisplay);
 
 ///*****END UI ELEMENTS*****///
 
-
+//This is essentially the game loop.
 function incrementClickCount(amtToInc: number): void {
-  clickCount += amtToInc;
-  clickCountDisplay.innerHTML = `You've tapped the turtle ${clickCount.toFixed(2)} times.`;
+    clickCount += amtToInc;
+    clickCountDisplay.innerHTML = `You've tapped the turtle ${clickCount.toFixed(2)} times.`;
+    checkButtonStates();
 }
 
 function autoIncrementClickCounter(lastUpdate: number = 0) {
-  const timestamp = performance.now();
-  if (lastUpdate !== 0) {
-    const elapsed = timestamp - lastUpdate;
-    const increment = elapsed / 1000; // elapsed is in milliseconds
-    incrementClickCount(increment);
-  }
-  requestAnimationFrame(() => autoIncrementClickCounter(timestamp));
+    const timestamp = performance.now();
+    if (lastUpdate !== 0) {
+        incrementClickCount(((timestamp - lastUpdate) / 1000) * growthRate);
+    }
+    requestAnimationFrame(() => autoIncrementClickCounter(timestamp));
 }
 
+function checkButtonStates() {
+    marioUpgradeButton.disabled = clickCount < 10;
+}
 
+//Start the game loop
 autoIncrementClickCounter();
 
