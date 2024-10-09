f95202401a17c65a9aaf8f688ffe1aca217e5204
Thomas Wessel <turpleturtle12@gmail.com>
2024-10-08 20:07:49 -0700

    Code cleanup
    Why did this execute *after* I pushed... whatever

--- src/main.ts
+++ src/main.ts
@@ -8,63 +8,67 @@ let clickCount = 0; //The number of times the turtle has been clicked
 let autoClickRate = 0; //The rate at which the turtle is clicked automatically
 
 interface Item {
-    upgradeName: string;
-    upgradeDescription: string;
-    currentCost: number;
-    growthRate: number;
-    purchaseCount: number;
-    specialAction?: () => void;
+  upgradeName: string;
+  upgradeDescription: string;
+  currentCost: number;
+  growthRate: number;
+  purchaseCount: number;
+  specialAction?: () => void;
 }
 
 //Define the upgrades to be created automatically
 const availableItems: Item[] = [
-    {
-        upgradeName: "Plastic Straw",
-        upgradeDescription: "The turtles will come running out of the ocean when they see this",
-        currentCost: 10,
-        growthRate: 0.1,
-        purchaseCount: 0,
-
+  {
+    upgradeName: "Plastic Straw",
+    upgradeDescription:
+      "The turtles will come running out of the ocean when they see this",
+    currentCost: 10,
+    growthRate: 0.1,
+    purchaseCount: 0,
+  },
+  {
+    upgradeName: "Aquarist",
+    upgradeDescription: "These guys know a thing or two about turtles.",
+    currentCost: 100,
+    growthRate: 2,
+    purchaseCount: 0,
+  },
+  {
+    upgradeName: "Turtle Sanctuary",
+    upgradeDescription:
+      "After gaslighting the turtles into thinking the ocean is unsafe, they'll never want to leave!",
+    currentCost: 500,
+    growthRate: 50,
+    purchaseCount: 0,
+  },
+  {
+    upgradeName: "Mario",
+    upgradeDescription: "He is somewhat related to turtles, right?",
+    currentCost: 1000,
+    growthRate: 75,
+    purchaseCount: 0,
+  },
+  {
+    upgradeName: "Earth 2",
+    upgradeDescription:
+      "Who needs these turtles? Go to earth 2 with your current growth rate. Resets progress",
+    currentCost: 10000,
+    growthRate: 0,
+    purchaseCount: 0,
+    specialAction: () => {
+      clickCount = 0;
+      availableItems.forEach((item) => {
+        if (item.upgradeName === "Earth 2") {
+          return;
+        }
+        item.purchaseCount = 0;
+        const button = document.querySelector(
+          `button[id='upgradeButton-${item.upgradeName}']`,
+        ) as HTMLButtonElement;
+        button.innerHTML = `${item.upgradeName} (${item.purchaseCount})`;
+      }, 0);
     },
-    {
-        upgradeName: "Aquarist",
-        upgradeDescription: "These guys know a thing or two about turtles.",
-        currentCost: 100,
-        growthRate: 2,
-        purchaseCount: 0,
-    },
-    {
-        upgradeName: "Turtle Sanctuary",
-        upgradeDescription: "After gaslighting the turtles into thinking the ocean is unsafe, they'll never want to leave!",
-        currentCost: 500,
-        growthRate: 50,
-        purchaseCount: 0,
-    },
-    {
-        upgradeName: "Mario",
-        upgradeDescription: "He is somewhat related to turtles, right?",
-        currentCost: 1000,
-        growthRate: 75,
-        purchaseCount: 0,
-    },
-    {
-        upgradeName: "Earth 2",
-        upgradeDescription: "Who needs these turtles? Go to earth 2 with your current growth rate. Resets progress",
-        currentCost: 10000,
-        growthRate: 0,
-        purchaseCount: 0,
-        specialAction: () => {
-            clickCount = 0;
-            availableItems.forEach((item) => {
-                if (item.upgradeName === "Earth 2") {
-                    return;
-                }
-                item.purchaseCount = 0;
-                const button = document.querySelector(`button[id='upgradeButton-${item.upgradeName}']`) as HTMLButtonElement;
-                button.innerHTML = `${item.upgradeName} (${item.purchaseCount})`;
-            }, 0);
-        },
-    }
+  },
 ];
 
 ///*****BEGIN UI ELEMENTS*****///
@@ -76,7 +80,7 @@ const mainClickButton = document.createElement("button");
 mainClickButton.id = "mainClickButton";
 mainClickButton.innerHTML = "🐢";
 mainClickButton.addEventListener("click", () => {
-    clickCount++;
+  clickCount++;
 });
 app.append(mainClickButton);
 
@@ -91,41 +95,43 @@ app.append(growthRateDisplay);
 
 //Slowly increases the speed of the turtle spin as the growth rate increases
 function updateTurtleSpinRate(): void {
-    const mainClickButton = document.getElementById("mainClickButton");
-    if (mainClickButton && autoClickRate > 0) {
-        const newDuration = Math.max(600 / (1 + autoClickRate), .1);
-        mainClickButton.style.animationDuration = `${newDuration}s`;
-        console.log(newDuration);
-    }
+  const mainClickButton = document.getElementById("mainClickButton");
+  if (mainClickButton && autoClickRate > 0) {
+    const newDuration = Math.max(600 / (1 + autoClickRate), 0.1);
+    mainClickButton.style.animationDuration = `${newDuration}s`;
+    console.log(newDuration);
+  }
 }
 
 //Game loop
 function tick(lastCall: number = 0): void {
-    const callTime = performance.now();
+  const callTime = performance.now();
 
-    if (lastCall === 0) {
-        requestAnimationFrame(() => tick(callTime));
-        return;
-    }
-
-    const singleFrameTime = (callTime - lastCall) / 1000;
-
-    //Add the auto clicker growth rate to the click count
-    clickCount += singleFrameTime * autoClickRate;
-
-    //update stat counters
-    clickCountDisplay.innerHTML = `You've tapped the turtle ${clickCount.toFixed(2)} times.`;
-    growthRateDisplay.innerHTML = `Auto Tap Multiplier: ${autoClickRate.toFixed(1)}x`;
-
-    //update buttons based on the current click count
-    availableItems.forEach((item) => {
-        const button = document.querySelector(`button[id='upgradeButton-${item.upgradeName}']`) as HTMLButtonElement;
-        button.disabled = clickCount < item.currentCost;
-    });
-
-    updateTurtleSpinRate();
-
+  if (lastCall === 0) {
     requestAnimationFrame(() => tick(callTime));
+    return;
+  }
+
+  const singleFrameTime = (callTime - lastCall) / 1000;
+
+  //Add the auto clicker growth rate to the click count
+  clickCount += singleFrameTime * autoClickRate;
+
+  //update stat counters
+  clickCountDisplay.innerHTML = `You've tapped the turtle ${clickCount.toFixed(2)} times.`;
+  growthRateDisplay.innerHTML = `Auto Tap Multiplier: ${autoClickRate.toFixed(1)}x`;
+
+  //update buttons based on the current click count
+  availableItems.forEach((item) => {
+    const button = document.querySelector(
+      `button[id='upgradeButton-${item.upgradeName}']`,
+    ) as HTMLButtonElement;
+    button.disabled = clickCount < item.currentCost;
+  });
+
+  updateTurtleSpinRate();
+
+  requestAnimationFrame(() => tick(callTime));
 }
 
 //Executed at game start
@@ -134,34 +140,34 @@ document.title = gameName;
 
 //Automatically create buttons for each upgrade
 availableItems.forEach((item) => {
-    const button = document.createElement("button");
+  const button = document.createElement("button");
 
-    button.innerHTML = `
+  button.innerHTML = `
         <div>${item.upgradeName} (${item.purchaseCount})</div>
         <div style="font-size: 0.8em;">${item.upgradeDescription}</div>
         <div style="font-size: 0.8em;">Cost: ${item.currentCost.toFixed(2)}</div>
     `;
 
-    button.disabled = true;
-    button.title = `Purchase one ${item.upgradeName}`;
-    button.id = "upgradeButton-" + item.upgradeName;
+  button.disabled = true;
+  button.title = `Purchase one ${item.upgradeName}`;
+  button.id = "upgradeButton-" + item.upgradeName;
 
-    button.addEventListener("click", () => {
-        autoClickRate += item.growthRate;
-        clickCount -= item.currentCost;
-        item.currentCost = item.currentCost * 1.15;
-        button.innerHTML = `
+  button.addEventListener("click", () => {
+    autoClickRate += item.growthRate;
+    clickCount -= item.currentCost;
+    item.currentCost = item.currentCost * 1.15;
+    button.innerHTML = `
             <div>${item.upgradeName} (${++item.purchaseCount})</div>
             <div style="font-size: 0.8em;">${item.upgradeDescription}</div>
             <div style="font-size: 0.8em;">Cost: ${item.currentCost.toFixed(2)}</div>
         `;
 
-        if (item.specialAction) {
-            item.specialAction();
-        }
-    });
+    if (item.specialAction) {
+      item.specialAction();
+    }
+  });
 
-    app.append(button);
+  app.append(button);
 });
 
 //start the game loop
